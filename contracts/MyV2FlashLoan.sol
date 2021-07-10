84c579fb3757d10322b52d4fc6a9fe5bf414e5d2
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-10 16:07:16 +0300

    remove withdrawable dep from myV2flashLoad

--- contracts/MyV2FlashLoan.sol
+++ contracts/MyV2FlashLoan.sol
@@ -4,7 +4,7 @@ import "./aave/FlashLoanReceiverBaseV2.sol";
 import "./interfaces/v2/ILendingPoolAddressesProviderV2.sol";
 import "./interfaces/v2/ILendingPoolV2.sol";
 
-contract FlashloanV2 is FlashLoanReceiverBaseV2, Withdrawable {
+contract FlashloanV2 is FlashLoanReceiverBaseV2 {
 
     constructor(address _addressProvider) FlashLoanReceiverBaseV2(_addressProvider) public {}
 
@@ -77,14 +77,14 @@ contract FlashloanV2 is FlashLoanReceiverBaseV2, Withdrawable {
     /*
      *  Flash multiple assets 
      */
-    function flashloan(address[] memory assets, uint256[] memory amounts) public onlyOwner {
+    function flashloan(address[] memory assets, uint256[] memory amounts) public {
         _flashloan(assets, amounts);
     }
 
     /*
      *  Flash loan 1000000000000000000 wei (1 ether) worth of `_asset`
      */
-    function flashloan(address _asset) public onlyOwner {
+    function flashloan(address _asset) public {
         bytes memory data = "";
         uint amount = 1 ether;
 
