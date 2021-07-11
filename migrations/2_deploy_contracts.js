40e8040bdce3397f292faf79171c63f44c283e42
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-11 12:36:21 +0300

    try new deploy address for aave

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const Contract = artifacts.require("MyV2FlashLoan");
 
-const DEPLOY_ADDRESS = '0x88757f2f99175387aB4C6a4b3067c77A695b0349';
+// const DEPLOY_ADDRESS = '0x88757f2f99175387aB4C6a4b3067c77A695b0349';
+const DEPLOY_ADDRESS = '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5';
+
 
 module.exports = function(deployer) {
   deployer.deploy(Contract, DEPLOY_ADDRESS);
