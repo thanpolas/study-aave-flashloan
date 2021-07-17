fde1e138a55f8431e2938405221e4d4759d2b411
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-17 15:48:13 +0300

    switch to mainnet fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 const { env } = process;
 const privateKey = env.ACCOUNT_PRIVATE_KEY;
 const endpointInfuraUrl = env.INFURA_ENDPOINT;
+const mainnetUrl = env.MAINNET_ALCHEMY;
 
 module.exports = {
   solidity: "0.8.0",
@@ -16,7 +17,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: endpointInfuraUrl,
+        url: mainnetUrl,
       },
       accounts: [
         {
@@ -26,6 +27,7 @@ module.exports = {
       ],
     },
     kovan: {
+      from: "0x5AEB7d3aAe835b15f3f670cD70eFEeCc181123ba",
       url: endpointInfuraUrl,
       accounts: [privateKey],
     },
