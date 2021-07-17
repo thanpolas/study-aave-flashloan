d1d4cd33e47555c0a0891af318429c4767f2e1f6
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-17 15:48:27 +0300

    work on deploy script

--- scripts/01-deploy.js
+++ scripts/01-deploy.js
@@ -4,38 +4,49 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { ethers } = hre;
 
 const { network } = hre.hardhatArguments;
 
 console.log("target network:", network);
 
-let lendingPoolAddressesProviderAddress;
-switch (network) {
-  case "mainnet":
-  case "mainnet-fork":
-  case "localhost": // For Ganache mainnet forks
-    lendingPoolAddressesProviderAddress =
-      // "0x24a42fD28C976A61Df5D00D0599C34c4f90748c8"; // old address
-      "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5"; // kovan address
-    break;
-  case "ropsten":
-  case "ropsten-fork":
-    lendingPoolAddressesProviderAddress =
-      "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728";
-    break;
-  case "kovan":
-  case "kovan-fork":
-    lendingPoolAddressesProviderAddress =
-      "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5";
-    break;
-  default:
-    throw Error(
-      `Are you deploying to the correct network? (network selected: ${network})`
-    );
-}
+const script = (module.exports = {});
+
+script.getAaveLendingPoolProvider = (networkStr) => {
+  let lendingPoolAddressesProviderAddress;
+  switch (networkStr) {
+    case "mainnet":
+    case "mainnet-fork":
+    case "localhost": // For Ganache mainnet forks
+      lendingPoolAddressesProviderAddress =
+        "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5";
+      break;
+    case "kovan":
+    case "kovan-fork":
+      lendingPoolAddressesProviderAddress =
+        "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
+      break;
+    default:
+      throw Error(
+        `Are you deploying to the correct network? (network selected: ${network})`
+      );
+  }
+
+  return lendingPoolAddressesProviderAddress;
+};
+
+script.verifyAaveProvider = () => {
+  console.log(
+    "Checking validity of AAVE Lending Pool Provider contract:",
+    aaveLendingPoolProviderAddress
+  );
+  const aaveProviderMarketId =
+    await aaveLendingPoolProviderContract.getMarketId();
+  console.log("Validity confirmed, id:", aaveProviderMarketId);
+};
 
 // AAVE Lending Pool
-const AAVE_LENDING_POOL = lendingPoolAddressesProviderAddress;
+const AAVE_LENDING_POOL = script.getAaveLendingPoolProvider(network);
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -44,9 +55,13 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  const signer = await ethers.getSigner();
+
+  console.log("signer.address:", signer.address);
 
   // We get the contract to deploy
   const Contract = await hre.ethers.getContractFactory("FlashloanV2");
+  console.log("Deploying using AAVE_LENDING_POOL:", AAVE_LENDING_POOL);
   const contract = await Contract.deploy(AAVE_LENDING_POOL);
 
   await contract.deployed();
