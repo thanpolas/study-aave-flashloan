b0c239efeead09bf23d0ca4d54fee2ddc19db96a
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-17 16:47:26 +0300

    refactor deploy and run-flashloan with single quotes

--- scripts/01-deploy.js
+++ scripts/01-deploy.js
@@ -3,46 +3,65 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat');
+const aaveLendingPoolProviderAbi = require('./abi/aave-lending-Pool-provider-v2.abi.json');
+
 const { ethers } = hre;
 
 const { network } = hre.hardhatArguments;
 
-console.log("target network:", network);
+console.log('target network:', network);
 
 const script = (module.exports = {});
 
 script.getAaveLendingPoolProvider = (networkStr) => {
   let lendingPoolAddressesProviderAddress;
   switch (networkStr) {
-    case "mainnet":
-    case "mainnet-fork":
-    case "localhost": // For Ganache mainnet forks
+    case 'mainnet':
+    case 'mainnet-fork':
+    case 'localhost': // For mainnet forks
       lendingPoolAddressesProviderAddress =
-        "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5";
+        '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5';
       break;
-    case "kovan":
-    case "kovan-fork":
+    case 'kovan':
+    case 'kovan-fork':
       lendingPoolAddressesProviderAddress =
-        "0x88757f2f99175387ab4c6a4b3067c77a695b0349";
+        '0x88757f2f99175387ab4c6a4b3067c77a695b0349';
       break;
     default:
       throw Error(
-        `Are you deploying to the correct network? (network selected: ${network})`
+        `Are you deploying to the correct network? (network selected: ${network})`,
       );
   }
 
   return lendingPoolAddressesProviderAddress;
 };
 
-script.verifyAaveProvider = () => {
-  console.log(
-    "Checking validity of AAVE Lending Pool Provider contract:",
-    aaveLendingPoolProviderAddress
+/**
+ * Verify that AAVE LP Provider contract exists and is working.
+ *
+ * @return {Promise}
+ */
+script.verifyAaveProvider = async () => {
+  const signer = await ethers.getSigner();
+  const aaveLendingPoolProviderAddress =
+    script.getAaveLendingPoolProvider(network);
+
+  const aaveLendingPoolProviderContract = new ethers.Contract(
+    aaveLendingPoolProviderAddress,
+    aaveLendingPoolProviderAbi,
+    signer,
   );
+
+  console.log(
+    'Checking validity of AAVE Lending Pool Provider contract:',
+    aaveLendingPoolProviderAddress,
+  );
+
   const aaveProviderMarketId =
     await aaveLendingPoolProviderContract.getMarketId();
-  console.log("Validity confirmed, id:", aaveProviderMarketId);
+
+  console.log('Validity confirmed, id:', aaveProviderMarketId);
 };
 
 // AAVE Lending Pool
@@ -57,16 +76,18 @@ async function main() {
   // await hre.run('compile');
   const signer = await ethers.getSigner();
 
-  console.log("signer.address:", signer.address);
+  console.log('signer.address:', signer.address);
+
+  await script.verifyAaveProvider();
 
   // We get the contract to deploy
-  const Contract = await hre.ethers.getContractFactory("FlashloanV2");
-  console.log("Deploying using AAVE_LENDING_POOL:", AAVE_LENDING_POOL);
+  const Contract = await hre.ethers.getContractFactory('FlashloanV2');
+  console.log('Deploying using AAVE_LENDING_POOL:', AAVE_LENDING_POOL);
   const contract = await Contract.deploy(AAVE_LENDING_POOL);
 
   await contract.deployed();
 
-  console.log("AAVE Flash Loan Contract deployed to:", contract.address);
+  console.log('AAVE Flash Loan Contract deployed to:', contract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/02-run-flashloan.js
+++ scripts/02-run-flashloan.js
@@ -2,35 +2,24 @@
  * @fileoverview Execute the deployed contract.
  */
 
-const util = require("util");
+require('dotenv').config();
 
-require("dotenv").config();
-const hre = require("hardhat");
-const { network } = hre.hardhatArguments;
+const hre = require('hardhat');
 
-const erc20abi = require("./abi/erc20.abi");
-const aaveLendingPoolProviderAbi = require("./abi/aave-lending-Pool-provider-v2.abi.json");
-
-const { getAaveLendingPoolProvider } = require("./01-deploy");
+const erc20abi = require('./abi/erc20.abi.json');
 
 const { env } = process;
 const { ethers } = hre;
-const KOVAN_WETH = "0xd0a1e359811322d97991e03f863a0c30c2cf029c";
+const KOVAN_WETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c';
 
 async function main() {
-  const aaveLendingPoolProviderAddress = getAaveLendingPoolProvider(network);
-  const artifacts = await hre.artifacts.readArtifact("FlashloanV2");
+  const artifacts = await hre.artifacts.readArtifact('FlashloanV2');
   const signer = await ethers.getSigner();
   const wethContract = new ethers.Contract(KOVAN_WETH, erc20abi, signer);
   const flashloanContract = new ethers.Contract(
     env.CONTRACT_DEPLOYED_ADDRESS,
     artifacts.abi,
-    signer
-  );
-  const aaveLendingPoolProviderContract = new ethers.Contract(
-    aaveLendingPoolProviderAddress,
-    aaveLendingPoolProviderAbi,
-    signer
+    signer,
   );
 
   const balance = await wethContract.balanceOf(env.CONTRACT_DEPLOYED_ADDRESS);
@@ -38,36 +27,36 @@ async function main() {
   const signerBalance = Number(await signer.getBalance());
   const owner = await flashloanContract.owner();
 
-  console.log("Deployed contract:", env.CONTRACT_DEPLOYED_ADDRESS);
-  console.log("balace of deployed contract:", contractBalance);
+  console.log('Deployed contract:', env.CONTRACT_DEPLOYED_ADDRESS);
+  console.log('balace of deployed contract:', contractBalance);
 
-  console.log("owner of deployed contract", owner);
+  console.log('owner of deployed contract', owner);
   console.log("Signer account's address:", signer.address);
   console.log("Signer account's balance:", signerBalance);
   try {
     if (true || contractBalance === 0) {
-      const transferSum = ethers.utils.parseEther("0.01");
+      const transferSum = ethers.utils.parseEther('0.01');
       console.log(`Transfering ${transferSum} WETH`);
       const tx = await wethContract.transfer(
         env.CONTRACT_DEPLOYED_ADDRESS,
-        transferSum
+        transferSum,
       );
-      console.log("WETH SENT to contract!", tx);
+      console.log('WETH SENT to contract!', tx);
     }
 
     const tx = await flashloanContract.flashloan(wethContract.address);
-    console.log("blockNumber b4", tx.blockNumber);
+    console.log('blockNumber b4', tx.blockNumber);
     const receipt = await tx.wait();
-    console.log("blockNumber", tx.blockNumber);
+    console.log('blockNumber', tx.blockNumber);
   } catch (ex) {
-    console.error("FAILED TO RUN");
-    console.error("Error Name:", ex.name);
-    console.error("Error reason:", ex.reason);
-    console.error("Error code:", ex.code);
-    console.error("Error argument:", ex.argument);
-    console.error("Error parent:", ex.parent);
-    console.error("Error data:", ex.data);
-    console.log("Error Properties:", Object.keys(ex));
+    console.error('FAILED TO RUN');
+    console.error('Error Name:', ex.name);
+    console.error('Error reason:', ex.reason);
+    console.error('Error code:', ex.code);
+    console.error('Error argument:', ex.argument);
+    console.error('Error parent:', ex.parent);
+    console.error('Error data:', ex.data);
+    console.log('Error Properties:', Object.keys(ex));
   }
 }
 
