3dc39b9772fa983f91be0acfa847f84875af27cd
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-17 13:11:45 +0300

    better deployment script

--- scripts/01-deploy.js
+++ scripts/01-deploy.js
@@ -5,23 +5,37 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
-// switch(network) {
-//   case "mainnet":
-//   case "mainnet-fork":
-//   case "development": // For Ganache mainnet forks
-//       lendingPoolAddressesProviderAddress = "0x24a42fD28C976A61Df5D00D0599C34c4f90748c8"; break
-//   case "ropsten":
-//   case "ropsten-fork":
-//       lendingPoolAddressesProviderAddress = "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728"; break
-//   case "kovan":
-//   case "kovan-fork":
-//       lendingPoolAddressesProviderAddress = "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5"; break
-//   default:
-//       throw Error(`Are you deploying to the correct network? (network selected: ${network})`)
-// }
+const { network } = hre.hardhatArguments;
+
+console.log("target network:", network);
+
+let lendingPoolAddressesProviderAddress;
+switch (network) {
+  case "mainnet":
+  case "mainnet-fork":
+  case "localhost": // For Ganache mainnet forks
+    lendingPoolAddressesProviderAddress =
+      // "0x24a42fD28C976A61Df5D00D0599C34c4f90748c8"; // old address
+      "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5"; // kovan address
+    break;
+  case "ropsten":
+  case "ropsten-fork":
+    lendingPoolAddressesProviderAddress =
+      "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728";
+    break;
+  case "kovan":
+  case "kovan-fork":
+    lendingPoolAddressesProviderAddress =
+      "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5";
+    break;
+  default:
+    throw Error(
+      `Are you deploying to the correct network? (network selected: ${network})`
+    );
+}
 
 // AAVE Lending Pool
-const AAVE_LENDING_POOL = "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5";
+const AAVE_LENDING_POOL = lendingPoolAddressesProviderAddress;
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
