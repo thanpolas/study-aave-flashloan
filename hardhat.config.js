1079680fcbe9b8e755a44eb9d992f22053808e91
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-17 13:11:31 +0300

    add localhost for hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,24 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 const { env } = process;
-const privateKey = env.GANACHE_PRIVATE_KEY;
+const privateKey = env.ACCOUNT_PRIVATE_KEY;
 const endpointInfuraUrl = env.INFURA_ENDPOINT;
 
 module.exports = {
   solidity: "0.8.0",
   defaultNetwork: "kovan",
   networks: {
-    hardhat: {},
+    hardhat: {
+      forking: {
+        url: endpointInfuraUrl,
+      },
+      accounts: [
+        {
+          privateKey,
+          balance: String(10e16),
+        },
+      ],
+    },
     kovan: {
       url: endpointInfuraUrl,
       accounts: [privateKey],
